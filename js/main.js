7bd1b9a12cb63ea987ac78528c5862f9d21879f8
Ahmetqaz <107630795+Ahmetqaz@users.noreply.github.com>
2023-05-19 22:31:45 +0500

    asdasdasdasdasdads

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+var menu = document.getElementById("menu");
+var menuBtn = document.getElementById("menuBtn");
+const header = document.getElementById("header");
+var body = document.body;
+
+const closeMenu = () => {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("active");
+  body.classList.remove("active");
+  header.classList.remove("active");
+};
+
+menuBtn.onclick = function () {
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  body.classList.toggle("active");
+  header.classList.toggle("active");
+};
+window.onclick = function (event) {
+  if (event.target == menu) {
+    closeMenu();
+  }
+};
+
+// if (header)
+//   window.addEventListener("scroll", () => {
+//     if (document.documentElement.scrollTop > 400) {
+//       header.classList.add("sticky");
+//     } else {
+//       header.classList.remove("sticky");
+//     }
+//   });
+///
+///
+/// slick slider
+
+///
+///
+/// tabEvents
+const toggleBody = (isClosed) => {
+  if (isClosed) {
+    document.body.classList.add("active");
+    if (menu) closeMenu();
+  } else {
+    document.body.classList.remove("active");
+  }
+};
+
 ///
 ///
 /// tabEvents
